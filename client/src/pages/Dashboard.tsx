1e2668aecedde63f7335ec0c91cda2939add546c
Roman <romalupan89@gmail.com>
2023-04-15 16:32:14 +0300

    added the files and folders needed

---
+++ client/src/pages/Dashboard.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Dashboard = () => {
+  /* Here I will render all the elements for the dashboard and so on for the rest on the screens */
+  return (
+    <div>Dashboard</div>
+  )
+}
+
+export default Dashboard
